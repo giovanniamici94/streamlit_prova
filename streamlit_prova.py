4413d151aec35be89cf0464dca61a26b58846f53
giovanniamici94 <63918896+giovanniamici94@users.noreply.github.com>
2024-11-28 12:23:07 +0100

    Create streamlit_prova.py

---
+++ streamlit_prova.py
@@ -0,0 +1,31 @@
+'''
+Run on terminal: streamlit run code.py, then a webpage will appear
+where you can re-run from there each time you update the Py code.
+Check streamlit website > Streamlit library > API reference
+to get a bunch of functionalities such as those here below
+Note: if you need to install anything, it might be necessary to put such
+package in this very folder.
+'''
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.write('# Main')
+
+st.write('Hello')
+answer = st.text_input('Favourite movie?')
+st.write(f'Ok, you just replied: {answer}')
+is_clicked = st.button('Click me')
+
+#st.link_button('Profile', url='/profile?id1234') #not working
+
+data = pd.read_csv('data.csv')
+st.write(data)
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns = ['a', 'b', 'c']
+)
+st.bar_chart(chart_data)
+st.line_chart(chart_data)
